fa62db7dae2ec7e6ef924dc669c9c4348fb13b58
Neil Ang <neilang@gmail.com>
2014-06-08 20:50:05 +1000

    Initial Commit

---
+++ Password Generator/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Password Generator
+//
+//  Created by Neil Ang on 8/06/2014.
+//  Copyright (c) 2014 Neil Ang. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
