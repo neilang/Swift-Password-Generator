6520d70d682fe1e5898fec8bd0b8cf612e04407d
Neil Ang <neilang@gmail.com>
2014-06-24 19:59:26 +1000

    Added swift code

--- Password Generator/AppDelegate.swift
+++ Password Generator/AppDelegate.swift
@@ -9,18 +9,43 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let password = PasswordGenerator()
                             
     @IBOutlet var window: NSWindow
+    @IBOutlet var passwordField : NSTextField
 
+    @IBOutlet var lettersSlider  : NSSlider
+    @IBOutlet var capitalsSlider : NSSlider
+    @IBOutlet var numbersSlider  : NSSlider
+    @IBOutlet var symbolsSlider  : NSSlider
+    
+    @IBOutlet var numbersField   : NSTextField
+    @IBOutlet var lowercaseField : NSTextField
+    @IBOutlet var uppercaseField : NSTextField
+    @IBOutlet var symbolsField   : NSTextField
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+        refreshPassword(self)
+    }
+    
+    func applicationShouldHandleReopen(theApplication: NSApplication!,hasVisibleWindows flag: Bool) -> Bool {
+        window.makeKeyAndOrderFront(self)
+        return true
     }
 
-    func applicationWillTerminate(aNotification: NSNotification?) {
-        // Insert code here to tear down your application
+    @IBAction func refreshPassword(sender : AnyObject) {
+        password.lowercase = lettersSlider.integerValue
+        password.uppercase = capitalsSlider.integerValue
+        password.numbers   = numbersSlider.integerValue
+        password.symbols   = symbolsSlider.integerValue
+
+        lowercaseField.stringValue = lettersSlider.integerValue.description
+        uppercaseField.stringValue = capitalsSlider.integerValue.description
+        numbersField.stringValue   = numbersSlider.integerValue.description
+        symbolsField.stringValue   = symbolsSlider.integerValue.description
+        passwordField.stringValue  = password.generate()
     }
-
-
+    
 }
 
---
+++ Password Generator/PasswordGenerator.swift
@@ -0,0 +1,69 @@
+//
+//  PasswordGenerator.swift
+//  Password Generator
+//
+//  Created by Neil Ang on 9/06/2014.
+//  Copyright (c) 2014 Neil Ang. All rights reserved.
+//
+import Foundation
+
+extension Array {
+    func randomItem() -> T? {
+        let idx = Int(arc4random()) % count
+        return self[idx]
+    }
+    
+    // Could contain duplicates
+    func randomItems(total: Int) -> T[] {
+        var result: T[] = []
+        for i in (0..total).by(1) {
+            if let item = randomItem() {
+                result += [item]
+            }
+        }
+        return result
+    }
+    
+    func shuffleItems() -> T[] {
+        var newArray = self
+        for i in (0..newArray.count) {
+            let j = Int(arc4random()) % newArray.count
+            newArray.insert(newArray.removeAtIndex(j), atIndex: i)
+        }
+        return newArray
+    }
+}
+
+extension String {
+    func split(bySeparator: String) -> Array<String> {
+        if countElements(bySeparator) < 1 {
+            var items: String[] = []
+            for c in self {
+                items.append(""+c)
+            }
+            return items
+        }
+        return self.componentsSeparatedByString(bySeparator)
+    }
+}
+
+class PasswordGenerator {
+    let lowercaseSet = "abcdefghijklmnopqrstuvwxyz".split("")
+    let uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    let symbolSet    = "!@#$%^&*?".split("")
+    let numberSet    = "0123456789".split("")
+    
+    var numbers   = 5
+    var lowercase = 5
+    var uppercase = 5
+    var symbols   = 5
+    
+    func generate() -> String {
+        var password: String[] = []
+        password += lowercaseSet.randomItems(lowercase)
+        password += uppercaseSet.randomItems(uppercase)
+        password += numberSet.randomItems(numbers)
+        password += symbolSet.randomItems(symbols)
+        return password.shuffleItems().reduce("") { (a, b) -> String in a+b }
+    }
+}
